52cb9c53e212b4a83af178171ddc6a78b6d4777e
Yarosh13 <115801847+Yarosh13@users.noreply.github.com>
2023-06-07 20:55:11 +0100

    Update paregens.c

--- paregens.c
+++ paregens.c
@@ -9,32 +9,48 @@
 #include "header.h"
 #include <time.h>
 
-int MenuParagem(Paragem* vetorParagens, int size){
+int MenuParagem(Paragem* vetorParagens, int *size){
     int escolha=999;
+    int sizeP=*size;
+    int el=0;
+
 
     while(1){
         srand(time(0));
         MenuParagens();
         printf("\n-=}");
+        fflush(stdin);
         scanf(" %d",&escolha);
 
         switch (escolha) {
             case 0:
                 printf("\nA voltar para o Menu Principal...\n");
-                sleep(2);
+                sleep(1);
                 return 0;
                 break;
 
             case 1:
-                AdicionarParagem(vetorParagens,size);
+                (*size)++;
+                vetorParagens = realloc(vetorParagens, sizeof(Paragem)* (*size));
+
+                AdicionarParagem(vetorParagens, size);
+
                 break;
 
             case 2:
-                EliminaParagem(vetorParagens,size);
+                el=EliminaParagem(vetorParagens,size);
+
+                if(el == 1){
+                    (*size)--;
+                    vetorParagens = realloc(vetorParagens, sizeof(Paragem)* (*size));
+
+                }
+
+
                 break;
 
             case 3:
-                MostrarParagens(vetorParagens,size);
+                MostrarParagens(vetorParagens,*size);
                 break;
 
             case 9:
@@ -55,7 +71,8 @@ int MenuParagens(){
     printf("1- ADICIONAR\n2- ELIMINAR\n3- Mostrar Paragens\n0- Voltar\n");
 }
 
-int InicializarParagem(Paragem* vetor, int size){
+/*
+int InicializarParagem(Paragem* vetor, int *size){
     //printf("%d",size);
     for (int i = 0; i < size; i++) {
         strcpy(vetor[i].nome, "NULL");
@@ -64,28 +81,23 @@ int InicializarParagem(Paragem* vetor, int size){
     }
     return 0;
 }
+*/
 
-int AdicionarParagem(Paragem* vetor, int size){
-    int novo;
-    for(int i=0; i<size;i++){
+int AdicionarParagem(Paragem* vetor, int *size){
+    int pos;
 
-        if(strcmp(vetor[i].id,"NULL") == 0){
-            novo=i;
-        }
-    }
+    pos = *size - 1;
     printf("\nIntroduza o nome da paragem: ");
-    scanf("%s",vetor[novo].nome);
+    scanf("%s",vetor[pos].nome);
 
-    GeraID(vetor,novo,size);
-
+    GeraID(vetor,pos, *size);
 }
 
 int MostrarParagens(Paragem* vetor, int size){
+
     for (int i = 0; i < size; ++i) {
-        if(strcmp(vetor[i].id,"NULL")!= 0){
             printf("\nID paragem: %s",vetor[i].id);
             printf("\nNome paragem: %s",vetor[i].nome);
-        }
     }
 }
 
@@ -99,7 +111,7 @@ int GeraID(Paragem* vetor,int pos, int size){
         vetor[pos].id[4] = '\0';  // Null-terminate the string
 
         for (int i = 0; i < size; ++i) {
-            if(strcmp(vetor[i].id,vetor[pos].id)!=0){
+            if(strcmp(vetor[i].id,vetor[pos].id)!=0 || size<=1){
                 return 1;
             }
         }
@@ -107,7 +119,7 @@ int GeraID(Paragem* vetor,int pos, int size){
 
 }
 
-int EliminaParagem(Paragem* vetor, int size){
+int EliminaParagem(Paragem* vetor, int *size){
     char ideliminar[5];
 
     printf("\nParagem a eliminar\n Introduza o ID: ");
@@ -117,8 +129,17 @@ int EliminaParagem(Paragem* vetor, int size){
     int sair=0;
     int eliminado=0;
     char sn;
+
+    for (int i = 0; i < *size; ++i) {
+        if(strcmp(vetor[i].id,ideliminar)==0 && vetor[i].associada==0){
+            printf("\nYES SIR!!");
+        }
+    }
+
+
     do{
         if(strcmp(vetor[cont].id,ideliminar)==0 && vetor[cont].associada==0){
+            printf("\n OK e igual!");
             //função elimina
             do {
                 printf("\nELIMINACAO DE UMA PARAGEM\n ID %s\n Nome;%s\n\nTem a certeza?(y/n)\n-=}",vetor[cont].id,vetor[cont].nome);
@@ -129,6 +150,22 @@ int EliminaParagem(Paragem* vetor, int size){
                     strcpy(vetor[cont].nome, "NULL");
                     strcpy(vetor[cont].id, "NULL");
                     vetor[cont].associada=0;
+
+                    for (int i = 0; i < *size; i++) {
+                        if (strcmp(vetor[i].id, "NULL") == 0) {
+                            for (int j = i; j < *size - 1; j++) {
+                                strcpy(vetor[j].nome, vetor[j + 1].nome);
+                                strcpy(vetor[j].id, vetor[j + 1].id);
+                                vetor[j].associada = vetor[j + 1].associada;
+                            }
+                            strcpy(vetor[*size - 1].nome, "NULL");
+                            strcpy(vetor[*size - 1].id, "NULL");
+                            vetor[*size - 1].associada = 0;
+                            (*size)--;
+                            i--;  // Process the new element at index i
+                        }
+                    }
+
                     sair=1;
                     break;
 
@@ -143,7 +180,7 @@ int EliminaParagem(Paragem* vetor, int size){
 
         }
 
-        if(cont == size){
+        if(cont == *size){
             sair=1;
             eliminado=1;
         }
@@ -151,38 +188,8 @@ int EliminaParagem(Paragem* vetor, int size){
     } while (sair !=1);
     if(eliminado == 1){
         printf("\nID nao existe!");
-
     }
 
-
-//    while((strcmp(vetor[i].id,"NULL")== 0)){
-//
-//        if(strcmp(vetor[i].id,"NULL")!= 0){
-//            printf("\nID paragem: %s",vetor[i].id);
-//            //função elimina
-//            printf("\nVou eliminar a paragem com o ID %s ",vetor[i].id);
-//            strcpy(vetor[i].nome, "NULL");
-//            strcpy(vetor[i].id, "NULL");
-//            vetor[i].associada=0;
-//
-//        }else{
-//            if(strcmp(vetor[i].id,ideliminar)==0 && vetor[i].associada != 0)
-//           i++;
-//        }
-//    }
+    return eliminado;
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
